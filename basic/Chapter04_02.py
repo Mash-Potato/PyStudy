74167bc86c2d9d91ee598912960dc63fcab19c0a
Rotomoo <quddlf410@naver.com>
2021-11-30 14:30:56 +0900

    test(Chapter04_02.py) : about for of repetitive statement

--- basic/Chapter04_02.py
+++ basic/Chapter04_02.py
@@ -4,7 +4,7 @@
 
 # 코딩의 핵심
 # for in <Collection>
-#   <loop body>
+#     <loop body>
 
 for v1 in range(10):  # 0 ~ 9
     print('v1 is :', v1)
@@ -146,4 +146,4 @@ name2 = 'Aceman'
 print('Reversed :', reversed(name2))
 print('List :', list(reversed(name2)))
 print('Tuple :', tuple(reversed(name2)))
-print('Set :', set(reversed(name2))) # 순서x, 중복x
+print('Set :', set(reversed(name2)))  # 순서x, 중복x
