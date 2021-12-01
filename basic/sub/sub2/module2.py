85ad41d33dfcde24a647e7bd78ff7e7fa33fbab3
Rotomoo <quddlf410@naver.com>
2021-12-01 14:49:08 +0900

    test(module2.py) : module module_test of Chapter06_03

--- basic/sub/sub2/module2.py
+++ basic/sub/sub2/module2.py
@@ -7,6 +7,7 @@ def mod2_test1():
 	print ("Module2 -> Test1")
 	print("Path : ", inspect.getfile(inspect.currentframe()))
 
+
 def mod2_test2():
 	print ("Module2 -> Test2")
 	print("Path : ", inspect.getfile(inspect.currentframe()))
