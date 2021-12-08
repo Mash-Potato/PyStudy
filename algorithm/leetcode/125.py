b5b201a6fb2d927f944c67f1297fa65ba99d7e84
“rotomoo” <quddlf410@naver.com>
2021-12-08 19:29:55 +0900

    test(125.py) : add String

--- algorithm/leetcode/125.py
+++ algorithm/leetcode/125.py
@@ -3,7 +3,7 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        # 정규식으로 불피요한 문자 필터링
+        # 정규식으로 불필요한 문자 필터링
         s = re.sub('[^a-z0-9]', '', s)
         return s == s[::-1]  #슬라이싱
 
