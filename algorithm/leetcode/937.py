0eb9f87f381fd02fd4062f5dac57383ae0fc4810
“rotomoo” <quddlf410@naver.com>
2021-12-08 16:29:40 +0900

    test(937.py) : add sort

---
+++ algorithm/leetcode/937.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        l_l = []
+        d_l = []
+        for log in logs:
+            if log.split()[1].isalpha():
+                l_l.append(log)
+            else:
+                d_l.append(log)
+
+        l_l.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return l_l + d_l
+
+
+print(Solution.reorderLogFiles(' ', ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]))
