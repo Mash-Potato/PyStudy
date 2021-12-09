11bf38631f576c0d9e637d82e5d43acee4465ce2
“rotomoo” <quddlf410@naver.com>
2021-12-09 15:19:54 +0900

    test(819.py) : add String

--- algorithm/leetcode/819.py
+++ algorithm/leetcode/819.py
@@ -11,27 +11,32 @@ from typing import List
 #         print(dict1)
 #         return dict1.most_common(1)[0][0]
 
-# class Solution:
-#     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-#         words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
-#         print(words)
-#         counts = collections.defaultdict(int)
-#         for word in words:
-#             counts[word] += 1
-#         print(counts)
-#         return max(counts, key=counts.get)
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+
+        # words 리스트에서 counts dictionary를 만들기
+        counts = collections.defaultdict(int)
+        for word in words:
+            counts[word] += 1
+
+        # 최대값
+        # print(max(counts.values()))
+
+        # 최대값을 가진 키
+        return max(counts, key=counts.get)
 
 
 # my code
-class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        paragraph = paragraph.lower()
-        paragraph = re.sub('[^\w]', ' ', paragraph).split()
-        print(paragraph)
-        paragraph = [x for x in paragraph if x not in banned]
-        # print(paragraph)
-        dict1 = collections.Counter(paragraph)
-        return dict1.most_common(1)[0][0]
+# class Solution:
+#     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+#         paragraph = paragraph.lower()
+#         paragraph = re.sub('[^\w]', ' ', paragraph).split()
+#         print(paragraph)
+#         paragraph = [x for x in paragraph if x not in banned]
+#         # print(paragraph)
+#         dict1 = collections.Counter(paragraph)
+#         return dict1.most_common(1)[0][0]
 
 print(Solution.mostCommonWord('',
 "a, a, a, a, b,b,b,c, c", ["a"]))
