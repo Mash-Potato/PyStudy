c83b253f3789b5a352e01e79d4b2fb29a63245d5
“rotomoo” <quddlf410@naver.com>
2021-12-09 15:32:40 +0900

    test(937.py) : add String

--- algorithm/leetcode/937.py
+++ algorithm/leetcode/937.py
@@ -1,17 +1,15 @@
 from typing import List
 
+
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         l_l = []
         d_l = []
         for log in logs:
-            if log.split()[1].isalpha():
-                l_l.append(log)
-            else:
+            if log.split()[1].isdigit():
                 d_l.append(log)
+            else:
+                l_l.append(log)
 
-        l_l.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-        return l_l + d_l
-
-
-print(Solution.reorderLogFiles(' ', ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]))
+        d_l.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return d_l + l_l
