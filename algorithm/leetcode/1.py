4ae0f21f2fd728473125313a452b0ba30b465064
“rotomoo” <quddlf410@naver.com>
2021-12-11 14:13:14 +0900

    test(1.py) : add arr(two sum)

--- algorithm/leetcode/1.py
+++ algorithm/leetcode/1.py
@@ -3,31 +3,30 @@ from typing import List
 # 키 조회 개선
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_dict = {}
+        n_dict = {}
         for i, num in enumerate(nums):
-            if target - num in nums_dict:
-                return [nums_dict[target - num], i]
-            nums_dict[num] = i
+            if target-num in n_dict:
+                return [n_dict[target-num], i]
+            n_dict[num] = i
 
 
 # 키 조회
 # class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         nums_dict = {}
+#         n_dict = {}
 #         for i, num in enumerate(nums):
-#             nums_dict[num] = i
+#             n_dict[num] = i
 #
 #         for i, num in enumerate(nums):
-#             if target - num in nums_dict and i != nums_dict[target - num]:
-#                 return [nums.index(num), nums_dict[target - num]]
+#             if target-num in n_dict and i != n_dict[target-num]:
+#                 return [nums.index(num), n_dict[target-num]]
 
 # in 탐색
 # class Solution:
 #     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         for i, v in enumerate(nums):
-#             res = target - v
-#             if res in nums[i+1:]:
-#                 return [nums.index(v), nums[i+1:].index(res)+(i+1)]
+#         for i, num in enumerate(nums):
+#             if target - num in nums[i+1:]:
+#                 return [nums.index(num), nums[i+1:].index(target-num)+(i+1)]
 
 # brute
 # class Solution:
