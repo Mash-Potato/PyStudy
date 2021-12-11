130eac6e9581164a15ec0746ddc15d68e4312da5
“rotomoo” <quddlf410@naver.com>
2021-12-11 14:25:01 +0900

    test(561.py) : add arr(pair)

---
+++ algorithm/leetcode/561.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        return sum(sorted(nums[::2]))
+
+# class Solution:
+#     def arrayPairSum(self, nums: List[int]) -> int:
+#         nums.sort()
+#         return sum(nums[0:len(nums):2])
