efd7ebd477066a71960da092eb04897a489a01c5
“rotomoo” <quddlf410@naver.com>
2021-12-13 18:08:55 +0900

    test(238.py) : add arr(multiply)

--- algorithm/leetcode/238.py
+++ algorithm/leetcode/238.py
@@ -17,3 +17,20 @@ class Solution:
         return answer
 
 print(Solution.productExceptSelf('', [1,2,3,4]))
+
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         answer = [1] * len(nums)
+#         answer2 = [1] * len(nums)
+#
+#         for i in range(1, len(nums)):
+#             answer[i] = answer[i-1] * nums[i-1]
+#
+#         for i in range(len(nums)-2, -1, -1):
+#             answer2[i] = answer2[i+1] * nums[i+1]
+#
+#         for i in range(len(nums)):
+#             answer[i] *= answer2[i]
+#
+#         return answer
