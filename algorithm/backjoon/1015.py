43a67cb6d415676515878d0a3406b647be2325dc
“rotomoo” <quddlf410@naver.com>
2021-12-15 12:22:59 +0900

    test(1015.py) : add arr sort

--- algorithm/backjoon/1015.py
+++ algorithm/backjoon/1015.py
@@ -1,12 +1,25 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-arr = list(map(int, input().split()))
-answer = [-1] * n
+a = list(map(int, input().split()))
+b = sorted(a)
+p = []
 for i in range(n):
-    idx = arr.index(min(arr))
-    answer[idx] = i
-    arr[idx] = 1001
+    p.append(b.index(a[i]))
+    b[p[i]] = 1001
 
-print(*answer)
+for x in p:
+    print(x, end=' ')
+
+print(*p)
+
+# import sys
+# input = sys.stdin.readline
+#
+# n = int(input())
+# arr = list(map(int, input().split()))
+# answer = [-1] * n
+# for i in range(n):
+#     idx = arr.index(min(arr))
+#     answer[idx] = i
+#     arr[idx] = 1001
+#
+# print(*answer)
