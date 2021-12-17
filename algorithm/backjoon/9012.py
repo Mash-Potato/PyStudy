34e5cb74e37e9b2bff69491fcea8de0efc15791e
“rotomoo” <quddlf410@naver.com>
2021-12-17 12:48:51 +0900

    test(9012.py) : add stack

--- algorithm/backjoon/9012.py
+++ algorithm/backjoon/9012.py
@@ -1,22 +1,43 @@
 import sys
-
 input = sys.stdin.readline
 
-n= int(input())
-while n > 0:
-    s = input().strip()
+n = int(input())
+for _ in range(n):
+    s = list(input().strip())
     s_dict = {')': '('}
     stack = []
-    flag = False
-    for x in s:
-        if x not in s_dict:
-            stack.append(x)
-        elif not stack or stack.pop() != s_dict[x]:
-            flag = True
-
-    if flag or len(stack) > 0:
-        print('NO')
+    for char in s:
+        if char not in s_dict:
+            stack.append(char)
+        elif not stack:
+            print('NO')
+            break
+        else:
+            stack.pop()
     else:
-        print('YES')
+        if stack:
+            print('NO')
+        else:
+            print('YES')
 
-    n -= 1
+# import sys
+# input = sys.stdin.readline
+#
+# n= int(input())
+# while n > 0:
+#     s = input().strip()
+#     s_dict = {')': '('}
+#     stack = []
+#     flag = False
+#     for x in s:
+#         if x not in s_dict:
+#             stack.append(x)
+#         elif not stack or stack.pop() != s_dict[x]:
+#             flag = True
+#
+#     if flag or len(stack) > 0:
+#         print('NO')
+#     else:
+#         print('YES')
+#
+#     n -= 1
