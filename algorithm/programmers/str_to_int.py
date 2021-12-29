d75d9668b5a4a50ea092c676091ff161449b5ed7
“rotomoo” <quddlf410@naver.com>
2021-12-29 16:13:15 +0900

    test(str_to_int.py) : add string

---
+++ algorithm/programmers/str_to_int.py
@@ -0,0 +1,5 @@
+def solution(s):
+    list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for i in range(10):
+        s = s.replace(list[i], str(i))
+    return int(s)
